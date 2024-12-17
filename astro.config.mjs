270e2c8d08ac0c33ab8709718e3129b02eaa0594
Elijah Kennedy <elijahclimbs@gmail.com>
2024-12-17 13:35:12 -0500

    added cloudflare adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	publicDir: 'public',
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  publicDir: 'public',
+  adapter: cloudflare(),
 });
+
+
+// export default defineConfig({
+//   integrations: [mdx(), sitemap()],
+//   publicDir: 'public',
+//   adapter: cloudflare({
+//     platformProxy: {
+//       enabled: true,
+//     },
+//   }),
+// });
